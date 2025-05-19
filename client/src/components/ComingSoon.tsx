9f4f8be61e1fc6e80f13617fb88dc32c7e7b2cad
Felipe Silva <felipe.silva@webtech87.pt>
2025-05-19 16:08:28 +0100

    [UPDATE] Adjust navbar position, add header and subtitle and background image

--- client/src/components/ComingSoon.tsx
+++ client/src/components/ComingSoon.tsx
@@ -1,10 +1,21 @@
 import styles from '../styles/ComingSoon.module.css'
+import bg_img from '../assets/logo-icon-background-accent.png'
 
 const ComingSoon = () => {
   return (
-    <div className={styles.home}>
-      
+    <div>
+        <div className={styles.home}>
+            <div>
+                <h1>MedSnap is Coming Soon...</h1>
+                <p>AI powered clinical diary revolutionizing aesthetic medicine and personalized treatments.</p>
+            </div>       
+        </div>
+        <div className={styles.bg}>
+            <img src={bg_img} alt="" />
+        </div>
+
     </div>
+    
   )
 }
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom'
 import styles from '../styles/Navbar.module.css'
+import logo from '../assets/logo.jpg'
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <Link to={'/'} className={styles.logo}>
-        <span>MedSnap</span>
-      </Link>
-      <button className={styles.join_button}>Join the wishlist</button>
+        <span>
+            <Link to={'/'} className={styles.logo}>
+                <img src={logo} alt="medsnap-logo" />
+            </Link>
+        </span>
+      
+        <button className={styles.join_button}>Join the wishlist</button>
     </nav>
   )
 }
