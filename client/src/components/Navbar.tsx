0e0bc1092af612311d5a1a149da39c5c7c4fa435
Felipe Silva <felipe.silva@webtech87.pt>
2025-05-22 11:59:12 +0100

    [UPDATE] Add language buttons to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
 import styles from '../styles/Navbar.module.css'
 import logo from '../assets/logo.jpg'
+import ptFlag from '../assets/flag-pt.png'
+import gbFlag from '../assets/flag-gb.png'
 
 const Navbar = () => {
   return (
@@ -10,8 +12,18 @@ const Navbar = () => {
                 <img src={logo} alt="medsnap-logo" />
             </Link>
         </span>
-      
-        <button className={styles.join_button}>Join the Waitlist</button>
+        <div className={styles.buttons}>
+          <button className={styles.join_button}>Join the Waitlist</button>
+          <div className={styles.lang_buttons}>
+            <button className={styles.pt_button}>
+              PT <img src={ptFlag} alt="PT" style={{width: '25px', height: '25px', margin: '0 0 0 5px'}}/>
+            </button>
+            <div className={styles.separator}></div>
+            <button className={styles.gb_button}>
+              EN <img src={gbFlag} alt="GB" style={{width: '25px', height: '25px', margin: '0 0 0 5px'}}/>
+            </button>            
+          </div>
+        </div>        
     </nav>
   )
 }
